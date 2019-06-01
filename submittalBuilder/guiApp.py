41b14f7f53bdabe18dcf461e4ff38ab596ae71c8
ecalzolaio <ecalzolaio@gmail.com>
2019-06-01 13:13:44 -0400

    Tied GUI to cover page builder, must test on windows

--- submittalBuilder/guiApp.py
+++ submittalBuilder/guiApp.py
@@ -1,26 +1,31 @@
 from tkinter import *
 from tkinter import filedialog
-
+from coverPageBuilder import buildCoverPage
 filename = None
 
 root = Tk()
 Label(root, text='Submittal No. ').grid(row=0)
 Label(root, text='Project Name ').grid(row=1)
 Label(root, text='Product Name ').grid(row=2)
-e1 = Entry(root)
-e2 = Entry(root)
-e3 = Entry(root)
-e1.grid(row=0, column=1)
-e2.grid(row=1, column=1)
-e3.grid(row=2, column=1)
+subNo = Entry(root)
+projName = Entry(root)
+prodName = Entry(root)
+subNo.grid(row=0, column=1)
+projName.grid(row=1, column=1)
+prodName.grid(row=2, column=1)
 
-def fileSelector():
-    global filename
-    root.withdraw()
-    root.fileName = filedialog.askopenfilename()
-    print(root.fileName)
+def selectAndProcess():
+    # root.fileName = filedialog.askopenfilename()
+    # print(root.fileName)
+    submittal_num = subNo.get()
+    project_name = projName.get()
+    product_name = prodName.get()
 
-Button(root, text='Select PDF file ', command=fileSelector).grid(row=3)
+    buildCoverPage(submittal_num, project_name, product_name)
+
+
+Button(root, text='Select PDF file ', command=selectAndProcess).grid(row=3)
+
 
 if __name__ == "__main__":
     mainloop()
